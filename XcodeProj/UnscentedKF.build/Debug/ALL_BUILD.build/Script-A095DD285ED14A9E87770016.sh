84733eeb07ca6992a8c75276133b26a2b285e4c6
liheng <liheng.lc.chen@gmail.com>
2017-06-18 09:14:17 +0200

    chili: object files generated

---
+++ XcodeProj/UnscentedKF.build/Debug/ALL_BUILD.build/Script-A095DD285ED14A9E87770016.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/Chili/Desktop/Proj2_Unscented_Kalman_Filter/Proj/CarND-Unscented-Kalman-Filter-Project/XcodeProj -f /Users/Chili/Desktop/Proj2_Unscented_Kalman_Filter/Proj/CarND-Unscented-Kalman-Filter-Project/XcodeProj/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
---
+++ XcodeProj/UnscentedKF.build/Debug/ZERO_CHECK.build/Script-3D3C47E7D1B94A71A8BD59C8.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/Chili/Desktop/Proj2_Unscented_Kalman_Filter/Proj/CarND-Unscented-Kalman-Filter-Project/XcodeProj -f /Users/Chili/Desktop/Proj2_Unscented_Kalman_Filter/Proj/CarND-Unscented-Kalman-Filter-Project/XcodeProj/CMakeScripts/ZERO_CHECK_cmakeRulesBuildPhase.make$CONFIGURATION all
